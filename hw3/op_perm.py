3f3084b0276f3e87150badfc7abc2805e771b525
nirefter2002 <nirefter@gmail.com>
2023-06-16 14:13:58 +0300

    Worked on Part 3 hook, getting code

--- hw3/op_perm.py
+++ hw3/op_perm.py
@@ -3,16 +3,9 @@ txt = f.readlines()
 f.close()
 org_perm = [chr(int("0x" + t.strip()[:2], 0)) for t in txt]
 
-
 perm = dict()
-printable = [chr(c) for c in range(ord(" "), ord("~") + 1)]
-printable_len = len(printable)
-#print(printable)
-#print(org_perm)
 for i in range(ord(" "), ord("~") + 1):
     perm[org_perm[i - ord(" ")]] = chr(i)
-#print(perm)
-
 
 inp = input()
 for c in inp:
